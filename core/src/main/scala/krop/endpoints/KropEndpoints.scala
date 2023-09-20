ada60b5a5ece616916d3719fca98c67692c86001
Noel Welsh <noel@noelwelsh.com>
2023-09-20 23:22:51 +0100

    Wrap routes in IO when constructed from endpoints

--- core/src/main/scala/krop/endpoints/KropEndpoints.scala
+++ core/src/main/scala/krop/endpoints/KropEndpoints.scala
@@ -180,7 +180,7 @@ trait EndpointsWithCustomErrors
           case NonFatal(t) => Some(handleServerError(http4sRequest, t))
         }
       }
-      Route(HttpRoutes.of(Function.unlift(route)))
+      Route(IO.pure(HttpRoutes.of(Function.unlift(route))))
     }
   }
 
