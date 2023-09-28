efaa24bda24ac6f0a3d977206d8469bd95a430bc
Noel Welsh <noel@noelwelsh.com>
2023-09-28 13:04:46 +0100

    Export literal string contexts correctly

--- core/src/main/scala/krop/all.scala
+++ core/src/main/scala/krop/all.scala
@@ -33,6 +33,49 @@ object all {
   export org.http4s.Method
   export org.http4s.Status
 
-  export com.comcast.ip4s.Literals.port
-  export com.comcast.ip4s.Literals.host
+  import com.comcast.ip4s.*
+  // Redefine these hear because I don't know how to export an anonymous extension method
+  extension (inline ctx: StringContext) {
+    inline def ip(inline args: Any*): IpAddress =
+      ${ Literals.ip('ctx, 'args) }
+
+    inline def ipv4(inline args: Any*): Ipv4Address =
+      ${ Literals.ipv4('ctx, 'args) }
+
+    inline def ipv6(inline args: Any*): Ipv6Address =
+      ${ Literals.ipv6('ctx, 'args) }
+
+    inline def mip(inline args: Any*): Multicast[IpAddress] =
+      ${ Literals.mip('ctx, 'args) }
+
+    inline def mipv4(inline args: Any*): Multicast[Ipv4Address] =
+      ${ Literals.mipv4('ctx, 'args) }
+
+    inline def mipv6(inline args: Any*): Multicast[Ipv6Address] =
+      ${ Literals.mipv6('ctx, 'args) }
+
+    inline def ssmip(
+        inline args: Any*
+    ): SourceSpecificMulticast.Strict[IpAddress] =
+      ${ Literals.ssmip('ctx, 'args) }
+
+    inline def ssmipv4(
+        inline args: Any*
+    ): SourceSpecificMulticast.Strict[Ipv4Address] =
+      ${ Literals.ssmipv4('ctx, 'args) }
+
+    inline def ssmipv6(
+        inline args: Any*
+    ): SourceSpecificMulticast.Strict[Ipv6Address] =
+      ${ Literals.ssmipv6('ctx, 'args) }
+
+    inline def port(inline args: Any*): Port =
+      ${ Literals.port('ctx, 'args) }
+
+    inline def host(inline args: Any*): Hostname =
+      ${ Literals.host('ctx, 'args) }
+
+    inline def idn(inline args: Any*): IDN =
+      ${ Literals.idn('ctx, 'args) }
+  }
 }
