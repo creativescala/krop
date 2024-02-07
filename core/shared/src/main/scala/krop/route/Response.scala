0370bb1ffae9eab166057de984e7b6bc3769c1fc
Noel Welsh <noel@noelwelsh.com>
2024-02-07 14:39:18 +0000

    More expressive `Response`
    - Add basic combinators for error handling and headers.
    - Add documentation

--- core/shared/src/main/scala/krop/route/Response.scala
+++ core/shared/src/main/scala/krop/route/Response.scala
@@ -19,79 +19,134 @@ package krop.route
 import cats.effect.IO
 import fs2.io.file.Path as Fs2Path
 import krop.KropRuntime
-import org.http4s.StaticFile
+import org.http4s.Header
+import org.http4s.Header.ToRaw
+import org.http4s.Headers
 import org.http4s.Status
 import org.http4s.dsl.io.*
 import org.http4s.Request as Http4sRequest
 import org.http4s.Response as Http4sResponse
+import org.http4s.StaticFile as Http4sStaticFile
 
 /** A [[krop.route.Response]] produces a [[org.http4s.Response]] given a value
   * of type A and a [[org.http4s.Request]].
   */
-sealed trait Response[A] {
+enum Response[A] {
   def respond(request: Http4sRequest[IO], value: A)(using
       runtime: KropRuntime
-  ): IO[Http4sResponse[IO]]
+  ): IO[Http4sResponse[IO]] =
+    this match {
+      case OrNotFound(source) =>
+        value match {
+          case Some(a) => source.respond(request, a)
+          case None    => Http4sResponse.notFoundFor(request)
+        }
 
-  def orNotFound: Response[Option[A]] =
-    Response.OrNotFound(this)
+      case OrElse(left, right) =>
+        value match {
+          case Left(a)  => left.respond(request, a)
+          case Right(b) => right.respond(request, b)
+        }
 
-  def orElse(that: Response[A]): Response[A] =
-    Response.OrElse(this, that)
+      case StaticResource(pathPrefix) =>
+        val path = pathPrefix ++ value
+        Http4sStaticFile
+          .fromResource(path, Some(request))
+          .getOrElseF(
+            IO(
+              runtime.logger.error(
+                s"Resource.staticResource couldn't load a resource from path $path."
+              )
+            ) *> InternalServerError()
+          )
 
-  def sum[O](that: Response[O]): Response[Either[A, O]] =
-    Response.Sum(this, that)
-}
+      case StaticDirectory(pathPrefix) =>
+        import runtime.given
 
-object Response {
-  case class StaticResource(pathPrefix: String) extends Response[String]:
-    /** Respond with a resource loaded by the Classloader. The `pathPrefix` is
-      * the prefix within the resources where the Classloader will look. E.g.
-      * "/krop/assets/". The `String` value is the rest of the resource name.
-      * E.g "krop.css".
-      */
-    override def respond(request: Http4sRequest[IO], fileName: String)(using
-        runtime: KropRuntime
-    ): IO[Http4sResponse[IO]] =
-      val path = pathPrefix ++ fileName
-      StaticFile
-        .fromResource(path, Some(request))
-        .getOrElseF(
-          IO(
-            runtime.logger.error(
-              s"Resource.staticResource couldn't load a resource from path $path."
-            )
-          ) *> InternalServerError()
+        Http4sStaticFile
+          .fromPath[IO](pathPrefix / value, Some(request))
+          .getOrElseF(InternalServerError())
+
+      case StaticFile(path) =>
+        import runtime.given
+
+        Http4sStaticFile
+          .fromPath[IO](path, Some(request))
+          .getOrElseF {
+            fs2.io.file.Files.forIO
+              .exists(path)
+              .map(exists =>
+                runtime.logger
+                  .error(
+                    s"""
+                       |Resource.staticFile couldn't load a file from path $path.
+                       |
+                       |  This path represents ${path.absolute} as an absolute path.
+                       |  A file ${
+                        if exists then "does" else "does not"
+                      } exist at this path.""".stripMargin
+                  )
+              )
+              *> InternalServerError()
+          }
+
+      case StatusEntityEncoding(status, entity) =>
+        IO.pure(
+          Http4sResponse(
+            status = status,
+            headers = entity.encoder.headers,
+            entity = entity.encoder.toEntity(value)
+          )
         )
 
-  case class OrNotFound[Z](source: Response[Z]) extends Response[Option[Z]]:
-    override def respond(request: Http4sRequest[IO], value: Option[Z])(using
-        runtime: KropRuntime
-    ): IO[Http4sResponse[IO]] =
-      value match
-        case Some(a) => source.respond(request, a)
-        case None    => IO.pure(Http4sResponse.notFound)
+      case WithHeader(source, headers) =>
+        source
+          .respond(request, value)
+          .map(response => response.withHeaders(headers))
+    }
 
-  case class OrElse[O](first: Response[O], second: Response[O])
-      extends Response[O]:
-    override def respond(request: Http4sRequest[IO], value: O)(using
-        runtime: KropRuntime
-    ): IO[Http4sResponse[IO]] =
-      first
-        .respond(request, value)
-        .orElse(second.respond(request, value))
+  /** Produce this `Response` if given `Some[A]`, otherwise produce a 404
+    * `Response`.
+    */
+  def orNotFound: Response[Option[A]] =
+    OrNotFound(this)
 
-  case class Sum[I, O](left: Response[I], right: Response[O])
-      extends Response[Either[I, O]]:
-    override def respond(request: Http4sRequest[IO], value: Either[I, O])(using
-        runtime: KropRuntime
-    ): IO[Http4sResponse[IO]] =
-      value match
-        case Left(b)  => left.respond(request, b)
-        case Right(a) => right.respond(request, a)
+  /** Produce this `Response` if given `Left[A]`, otherwise produce that
+    * `Response` given `Right[B]`.
+    */
+  def orElse[B](that: Response[B]): Response[Either[A, B]] =
+    OrElse(this, that)
 
+  /** Add headers to this Response. The headers can be any form that
+    * [[org.http4s.Header.ToRaw]] accepts, which is:
+    *
+    *   - A value of type `A` which has a `Header[A]` in scope
+    *   - A (name, value) pair of `String`, which is treated as a `Recurring`
+    *     header
+    *   - A `Header.Raw`
+    *   - A `Foldable` (`List`, `Option`, etc) of the above.
+    */
+  def withHeader(header: ToRaw, headers: ToRaw*): Response[A] =
+    WithHeader(this, Headers(header.values ++ headers.flatMap(_.values)))
+
+  case OrNotFound[A](source: Response[A]) extends Response[Option[A]]
+  case OrElse[A, B](left: Response[A], right: Response[B])
+      extends Response[Either[A, B]]
+  case StaticResource(pathPrefix: String) extends Response[String]
+  case StaticDirectory(pathPrefix: Fs2Path) extends Response[Fs2Path]
+  case StaticFile(path: Fs2Path) extends Response[Unit]
+  case StatusEntityEncoding(status: Status, entity: Entity[?, A])
+  case WithHeader(source: Response[A], header: Headers)
+}
+object Response {
+
+  /** Respond with a resource loaded by the Classloader. The `pathPrefix` is the
+    * prefix within the resources where the Classloader will look. E.g.
+    * "/krop/assets/". The `String` value is the rest of the resource name. E.g
+    * "krop.css".
+    */
   def staticResource(pathPrefix: String): Response[String] =
-    StaticResource(pathPrefix)
+    Response.StaticResource(pathPrefix)
 
   /** Respond with a file loaded from the filesystem. The `pathPrefix` is the
     * directory within the file system where the files will be found. E.g.
@@ -99,79 +154,20 @@ object Response {
     * "krop.css".
     */
   def staticDirectory(pathPrefix: Fs2Path): Response[Fs2Path] =
-    new Response[Fs2Path] {
-      def respond(
-          request: Http4sRequest[IO],
-          fileName: Fs2Path
-      )(using runtime: KropRuntime): IO[Http4sResponse[IO]] = {
-        import runtime.given
-
-        StaticFile
-          .fromPath[IO](pathPrefix / fileName, Some(request))
-          .getOrElseF(InternalServerError())
-      }
-    }
+    Response.StaticDirectory(pathPrefix)
 
   /** Respond with a file loaded from the filesystem. The `path` is the location
     * of the file. E.g. "/etc/assets/index.html".
     */
   def staticFile(path: String): Response[Unit] =
-    new Response[Unit] {
-      def respond(
-          request: Http4sRequest[IO],
-          unit: Unit
-      )(using runtime: KropRuntime): IO[Http4sResponse[IO]] = {
-        import runtime.given
+    Response.StaticFile(fs2.io.file.Path(path))
 
-        val p = fs2.io.file.Path(path)
-
-        StaticFile
-          .fromPath[IO](p, Some(request))
-          .getOrElseF {
-            fs2.io.file.Files.forIO
-              .exists(p)
-              .map(exists =>
-                runtime.logger
-                  .error(
-                    s"""
-                       |Resource.staticFile couldn't load a file from path $path.
-                       |
-                       |  This path represents ${p.absolute} as an absolute path.
-                       |  A file ${
-                        if exists then "does" else "does not"
-                      } exist at this path.""".stripMargin
-                  )
-              )
-              *>
-                InternalServerError()
-          }
-      }
-    }
+  def notFound[A](entity: Entity[?, A]): Response[A] =
+    status(Status.NotFound, entity)
 
   def ok[A](entity: Entity[?, A]): Response[A] =
     status(Status.Ok, entity)
 
   def status[A](status: Status, entity: Entity[?, A]): Response[A] =
-    StatusEntityEncodingResponse(status, entity)
-
-  /** A [[krop.route.Response]] that specifies only a HTTP status code and an
-    * [[org.http4s.EntityEncoder]].
-    */
-  final case class StatusEntityEncodingResponse[A](
-      status: Status,
-      entity: Entity[?, A]
-  ) extends Response[A] {
-    def respond(
-        request: Http4sRequest[IO],
-        value: A
-    )(using runtime: KropRuntime): IO[Http4sResponse[IO]] =
-      IO.pure(
-        Http4sResponse(
-          status = status,
-          headers = entity.encoder.headers,
-          entity = entity.encoder.toEntity(value)
-        )
-      )
-  }
-
+    Response.StatusEntityEncoding(status, entity)
 }
--- docs/src/pages/routes/response.md
+++ docs/src/pages/routes/response.md
@@ -4,10 +4,47 @@
 import krop.all.*
 ```
 
-A @:api(krop.route.Response) describes how a HTTP response can be constructed from Scala values. A response can be anything that implements the `Response` trait, but the usual way to call one of the constructors on the @:api(krop.route.Response$) companion object. For example
+A @:api(krop.route.Response) describes how to create a HTTP response from a Scala value. 
+For example, the following `Response` will produce an HTTP OK response with a `text/plain` entity.
 
 ```scala mdoc:silent
-val response = Response.ok(Entity.html)
+val ok: Response[String] = Response.ok(Entity.text)
 ```
 
-constructs a `Response` that responds with an HTTP OK, and an HTML entity constructed from a Scala `String`.
+The entity will be constructed from a `String` that is passed to the `respond` method on the `Response`.
+You usually won't do this yourself; it is handled by the `Route` the `Response` is part of.
+
+
+## Entities
+
+Entities (response bodies) are handled in the same way as [requests](request.md): by specifying an @:api(krop.route.Entity). In this case the `Entity` is responsible for encoding Scala values as data in the HTTP response.
+
+Use `Entity.unit` to indicate that your response has no entity. For example:
+
+```scala mdoc:silent
+val noBody: Response[Unit] = Response.ok(Entity.unit)
+```
+
+
+## Headers
+
+Headers can be added using the `withHeader` method. This method accepts one of more values in any of the following forms:
+
+- A value of type `A` which has a `Header[A]` in scope
+- A `(name, value)` pair of `String`, which is treated as a `Recurring`
+  header
+- A `Header.Raw`
+- A `Foldable` (`List`, `Option`, etc) of the above.
+
+In the example below we add a header using the `(name, value)` form, and a value with a `Header[A]` in scope.
+
+```scala mdoc:silent
+import org.http4s.headers.Allow
+
+val headers = Response.ok(Entity.html).withHeader("X-Awesomeness" -> "10.0", Allow(Method.GET))
+```
+
+
+## Error Handling
+
+In many cases you'll need to generate an error response despite a valid request. For example, you would generate a 404 Not Found if the client has sent a well-formed request but the requested resource doesn't exist. This situation can be handled using the `orNotFound` method, which converts a `Response[A]` to a `Response[Option[A]]`. When passed a `None` the `Response` responds with a 404. For more complex cases you can use `orElse`, which allows you to handle an `Either[A, B]` and introduce custom error handling.
