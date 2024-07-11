3f307a8b495bf3834f77b65cf6e79371933250e2
Noel Welsh <noel@noelwelsh.com>
2024-07-11 06:44:32 +0100

    Add header

--- core/jvm/src/test/scala/krop/route/RequestHeaderSuite.scala
+++ core/jvm/src/test/scala/krop/route/RequestHeaderSuite.scala
@@ -1,14 +1,30 @@
+/*
+ * Copyright 2023 Creative Scala
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package krop.route
 
 import krop.raise.Raise
 import munit.CatsEffectSuite
+import org.http4s.Headers
+import org.http4s.MediaType
 import org.http4s.Method
 import org.http4s.Uri
+import org.http4s.headers.`Content-Type`
 import org.http4s.implicits.*
 import org.http4s.{Request as Http4sRequest}
-import org.http4s.headers.`Content-Type`
-import org.http4s.MediaType
-import org.http4s.Headers
 
 class RequestHeaderSuite extends CatsEffectSuite {
   test("Ensure header fails if header does not exist") {
