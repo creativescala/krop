8de3c2a887ef3927b4aff50881cad2d5cc3f4300
Noel Welsh <noel@noelwelsh.com>
2025-04-03 16:10:36 +0100

    Formatting and header

--- core/shared/src/main/scala/krop/route/FormCodec.scala
+++ core/shared/src/main/scala/krop/route/FormCodec.scala
@@ -1,16 +1,31 @@
+/*
+ * Copyright 2023 Creative Scala
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package krop.route
 
-import org.http4s.UrlForm
+import cats.data.Chain
+import cats.effect.IO
+import krop.route.Param.All
+import krop.route.Param.One
 import org.http4s.DecodeFailure
+import org.http4s.UrlForm
 
 import scala.compiletime.*
 import scala.deriving.*
 import scala.quoted.*
-
-import cats.effect.IO
-import cats.data.Chain
-import krop.route.Param.All
-import krop.route.Param.One
 
 final case class FormCodec[A](
     decode: UrlForm => IO[Either[DecodeFailure, A]],
