8b7db48c17211b4d990df2a60cf7a731b5f64e56
Noel Welsh <noel@noelwelsh.com>
2025-04-24 20:07:56 +0100

    Add ScalaDoc for SeqStringCodec

--- core/shared/src/main/scala/krop/route/SeqStringCodec.scala
+++ core/shared/src/main/scala/krop/route/SeqStringCodec.scala
@@ -19,6 +19,8 @@ trait SeqStringCodec[A] {
   def encode(value: A): Seq[String]
 }
 object SeqStringCodec {
+
+  /** A [[SeqStringCodec]] that passes through its input unchanged. */
   given seqString: SeqStringCodec[Seq[String]] =
     new SeqStringCodec[Seq[String]] {
       val name: String = "Seq[String]"
@@ -31,6 +33,9 @@ object SeqStringCodec {
       def encode(value: Seq[String]): Seq[String] = value
     }
 
+  /** Constructs a [[SeqStringCodec]] from a given [[StringCodec]]. Decoding
+    * fails if no values are available.
+    */
   given fromStringCodec[A](using codec: StringCodec[A]): SeqStringCodec[A] =
     new SeqStringCodec[A] {
       val name: String = codec.name
@@ -46,6 +51,30 @@ object SeqStringCodec {
 
       def encode(value: A): Seq[String] =
         Seq(codec.encode(value))
+    }
+
+  /** Constructs a [[SeqStringCodec]] from a given [[StringCodec]]. Decoding
+    * returns None if no values are available; otherwise the values is Some of
+    * the decoded values.
+    */
+  given optional[A](using codec: StringCodec[A]): SeqStringCodec[Option[A]] =
+    new SeqStringCodec[Option[A]] {
+      val name: String = codec.name
+
+      def decode(
+          value: Seq[String]
+      ): Either[SeqStringDecodeFailure, Option[A]] =
+        value.headOption
+          .map(
+            codec
+              .decode(_)
+              .leftMap(SeqStringDecodeFailure.fromStringDecodeFailure)
+              .map(_.some)
+          )
+          .getOrElse(Right(None))
+
+      def encode(value: Option[A]): Seq[String] =
+        value.map(codec.encode).toSeq
     }
 }
 
