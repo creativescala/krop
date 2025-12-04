5546270bb89645344b8d67bb04631532d22ddea6
Noel Welsh <noel@noelwelsh.com>
2025-12-04 13:21:25 +0000

    Update dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,6 +8,7 @@ object Dependencies {
   val catsVersion = "2.10.0"
   val catsEffectVersion = "3.5.1"
   val circeVersion = "0.14.13"
+  val circeGenericVersion = "0.14.15"
   val declineVersion = "2.5.0"
   val fs2Version = "3.6.1"
   val http4sVersion = "1.0.0-M46"
@@ -15,9 +16,9 @@ object Dependencies {
   val scalaTagsVersion = "0.13.1"
   val twirlVersion = "2.0.9"
   val log4catsVersion = "2.7.1"
-  val logbackVersion = "1.5.20"
+  val logbackVersion = "1.5.21"
 
-  val sqliteVersion = "3.50.3.0"
+  val sqliteVersion = "3.51.1.0"
   val magnumVersion = "1.3.1"
 
   val directoryWatcherVersion = "0.19.1"
@@ -49,7 +50,8 @@ object Dependencies {
     Def.setting("org.http4s" %%% "http4s-ember-server" % http4sVersion)
   val http4sDsl = Def.setting("org.http4s" %%% "http4s-dsl" % http4sVersion)
   val http4sCirce = Def.setting("org.http4s" %%% "http4s-circe" % http4sVersion)
-  val circeGeneric = Def.setting("io.circe" %% "circe-generic" % circeVersion)
+  val circeGeneric =
+    Def.setting("io.circe" %% "circe-generic" % circeGenericVersion)
 
   val sqlite = Def.setting("org.xerial" % "sqlite-jdbc" % sqliteVersion)
   val magnum = Def.setting("com.augustnagro" %% "magnum" % magnumVersion)
