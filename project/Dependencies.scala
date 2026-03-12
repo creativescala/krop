e32ec412c68df7534da15c2824fb1c7ef5e0c3b1
Noel Welsh <noel@noelwelsh.com>
2026-03-12 14:54:22 +0000

    Update dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,23 +9,23 @@ object Dependencies {
   val catsEffectVersion = "3.5.1"
   val circeVersion = "0.14.13"
   val circeGenericVersion = "0.14.15"
-  val declineVersion = "2.5.0"
+  val declineVersion = "2.6.0"
   val fs2Version = "3.6.1"
   val http4sVersion = "1.0.0-M46"
   val scalaJsDomVersion = "2.4.0"
   val scalaTagsVersion = "0.13.1"
   val twirlVersion = "2.0.9"
-  val log4catsVersion = "2.7.1"
-  val logbackVersion = "1.5.21"
+  val log4catsVersion = "2.8.0"
+  val logbackVersion = "1.5.32"
 
-  val sqliteVersion = "3.51.1.0"
+  val sqliteVersion = "3.51.2.0"
   val magnumVersion = "1.3.1"
 
   val directoryWatcherVersion = "0.19.1"
   val betterFilesVersion = "3.9.2"
 
   val munitVersion = "0.7.29"
-  val munitCatsVersion = "2.1.0"
+  val munitCatsVersion = "2.2.0"
 
   // Libraries
   val catsEffect =
